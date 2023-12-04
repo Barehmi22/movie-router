862dabd4d3abed076e2feebf5f69506a918dcdd5
Salem <barhmisalem44.iss@gmail.com>
2023-12-04 15:46:40 +0100

    my first commit

---
+++ src/MovieList.js
@@ -0,0 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Row, Col } from 'react-bootstrap'
+import MovieCard from './MovieCard';
+import { Link } from "react-router-dom";
+
+function Movieslist({ moviesList, titlefilter, ratingfilter }) {    
+    return (
+        <Row xs={2} md={4} className="g-4">
+       
+                    movieElement.title.toLowerCase().includes(titlefilter.toLowerCase().trim()) &&
+                   ( movieElement.rating .ratingfilter()).map(movieElement, index) 
+                    (
+                <Col key={'index'} >
+                    <Link to={`/movie/${'index'}`}>
+                    <MovieCard movie={'movieElement'} />
+                    </Link>
+
+                </Col>)
+                </Row>
+            )}
+            
+   
+export default Movieslist;
